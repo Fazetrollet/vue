c6106a58ff701b31bc2b48e9f574010b441ca75b
Fazetrollet <154237719+Fazetrollet@users.noreply.github.com>
2024-10-09 09:43:28 +0300

    fixat resultatrad

--- ssp/src/App.vue
+++ ssp/src/App.vue
@@ -1,34 +1,16 @@
 <script setup>
 import { ref } from 'vue'
 import KnappRad from './components/KnappRad.vue';
+import ResultatRad from './components/ResultatRad.vue';
 
 const score = ref({ spelare: 0, dator: 0 })
-const resultat = ref('Du vann')
 const knappar = ref(['Sten', 'Sax', 'Påse']);
+const resultat = ref({});
 
-function hittaVinnare() {
-  let buttons = document.getElementsByClassName('alternativ')
-  for (let b of buttons) {
-    if (b.classList.contains('spelarval')) {
-      var spelarval = b.textContent
-    }
-    if (b.classList.contains('datorval')) {
-      var datorval = b.textContent
-    }
-  }
-  if (spelarval == datorval) {
-    resultat.value = 'Oavgjort!'
-  } else if (
-    (spelarval == 'Sten' && datorval == 'Sax') ||
-    (spelarval == 'Sax' && datorval == 'Påse') ||
-    (spelarval == 'Påse' && datorval == 'Sten')
-  ) {
-    resultat.value = 'Du vann?'
-    score.value.spelare++
-  } else {
-    resultat.value = 'Du förlorade!'
-    score.value.dator++
-  }
+function hittaVinnare(valdaKnappar) {
+ let spelare = knappar.value.indexOf(valdaKnappar.spelare)
+ let dator = knappar.value.indexOf(valdaKnappar.dator)
+ resultat.value = { spelare: spelare, dator: dator }
 }
 function reset() {
   score.value.spelare = 0
@@ -48,10 +30,7 @@ function reset() {
 
   <main>
     <KnappRad :knappar="knappar" @valda-knappar="hittaVinnare" />
-    
-    <div class="resultat">
-      <p id="resultat">{{ resultat }}</p>
-    </div>
+    <ResultatRad :valda-knappar="resultat" />
     <div class="score">
       <p>
         <span id="spelare"> {{ score.spelare }}</span> -
@@ -68,12 +47,6 @@ function reset() {
 header {
   text-align: center;
   margin-bottom: 1.2em;
-}
-
-.resultat {
-  font-size: 1.2em;
-  text-align: center;
-  margin: 1.2em 0;
 }
 .score {
   font-size: 1.2em;
